0fb236360bad169deff79eac90d5aa4f6e1bffbf
Kae Anderson <andersonkaem@gmail.com>
2017-03-10 09:45:38 -0600

    main part of index route

--- app/routes/application.vue
+++ app/routes/application.vue
@@ -2,23 +2,18 @@
   <div class="app">
     <nav class="nav">
       <div class="nav-left">
-        <!--<router-link v-bind:to="{ name: 'index' }">Puppies</router-link>-->
+        <!--<router-link class="nav-item" :to="{ name: 'index' }">Puppies</router-link>-->
         <a class="nav-item">
             Puppies
           </a>
 
-
-
-
-        <!-- This "nav-menu" is hidden on mobile -->
-        <!-- Add the modifier "is-active" to display it on mobile -->
         <div class="nav-right">
-          <!--<router-link v-bind:to="{ name: 'index' }">All Puppies</router-link>-->
+          <!--<router-link class="nav-item" :to="{ name: 'index' }">Puppies</router-link>-->
           <a class="nav-item">
           All Puppies
           </a>
 
-          <!--<router-link v-bind:to="{ name: 'new' }">Add Puppy</router-link>-->
+          <!--<router-link class="nav-item" :to="{ name: 'index' }">Puppies</router-link>-->
           <a class="nav-item">
           Add Puppy
           </a>
@@ -27,8 +22,9 @@
         </div>
     </nav>
     <div class="container">
-      <div class="column">
-        <div class="column is-4">
+      <div class="section">
+      <div class="columns">
+        <div class="column is-one-third">
           <nav class="panel">
             <p class="panel-heading">
               Adopt A Pupper
@@ -37,11 +33,11 @@
               <div class="media">
                 <div class="media-left">
                   <div class="image is-64x64">
-                    <img src="http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-21.jpg" alt="">
+                    <img src="http://cdn1-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-2.jpg" alt="">
                   </div>
                 </div>
                 <div class="media-right">
-                  <div class="subtitle">Luna</div>
+                  <div class="subtitle">Bob</div>
                   <div class="subtitle">read more
                     <!--<a href="#"></a>-->
                   </div>
@@ -67,11 +63,11 @@
               <div class="media">
                 <div class="media-left">
                   <div class="image is-64x64">
-                    <img src="http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-21.jpg" alt="">
+                    <img src="http://cdn1-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-2.jpg" alt="">
                   </div>
                 </div>
                 <div class="media-right">
-                  <div class="subtitle">Luna</div>
+                  <div class="subtitle">Bob</div>
                   <div class="subtitle">read more
                     <!--<a href="#"></a>-->
                   </div>
@@ -94,18 +90,90 @@
               </div>
             </div>
 
-          </nav>
-        </div> <!--is 4-->
 
-      <div class="column is-8">
-        <div class="title has-text-centered">Luna</div>
+        </div> <!--is 1/3-->
 
 
+      <div class="column is-two-thirds">
+        <div class="level">
+          <div class="title level-left">Puppies For Adoption</div>
+          <a class="button is-info level-right">this doesn't work yet!</a>
+          </div>
+          <table class="table">
+            <thead>
+              <tr>
+                <th><abbr title="Name">Name</abbr></th>
+                <th><abbr title="Age">Age</abbr></th>
+                <th><abbr title="Read"></abbr></th>
+              </tr>
+            </thead>
 
+            <tbody>
+              <tr>
+                <td>Bob</td>
+                <td>1</td>
+                <td>read more</td>
+              </tr>
+              <tr>
+                <td>Luna</td>
+                <td>1</td>
+                <td>read more</td>
+              </tr>
+              <tr>
+                <td>Bob</td>
+                <td>1</td>
+                <td>read more</td>
+              </tr>
+              <tr>
+                <td>Luna</td>
+                <td>1</td>
+                <td>read more</td>
+              </tr>
+
+
+            </tbody>
+          </table>
+          <div class="title">Adopted Puppers</div>
+          <table class="table">
+            <thead>
+              <tr>
+                <th><abbr title="Name">Name</abbr></th>
+                <th><abbr title="Age">Age</abbr></th>
+                <th><abbr title="Read"></abbr></th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td>Bob</td>
+                <td>1</td>
+                <td>read more</td>
+              </tr>
+              <tr>
+                <td>Luna</td>
+                <td>1</td>
+                <td>read more</td>
+              </tr>
+              <tr>
+                <td>Bob</td>
+                <td>1</td>
+                <td>read more</td>
+              </tr>
+              <tr>
+                <td>Luna</td>
+                <td>1</td>
+                <td>read more</td>
+              </tr>
+
+
+            </tbody>
+          </table>
+        </div> <!--2/3-->
+
+
+
+      </div> <!--columns-->
       </div>
-
-
-      </div> <!--column-->
     </div> <!--container-->
 </template>
 
